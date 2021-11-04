ccb5da5d39525d909c4758aa3f144dff12cb35ac
Paulo Vidal <u1d4lp@gmail.com>
2021-11-04 20:17:30 -0300

    convert Tasks into mapping, implement event for task creation

--- contracts/ToDo.sol
+++ contracts/ToDo.sol
@@ -2,25 +2,62 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract ToDo {
-  struct Task {
-    uint id;
-    uint date;
-    string content;
-    string author;
-    bool done;
-  }
+   struct Task {
+      uint256 id;
+      uint256 date;
+      string content;
+      string author;
+      bool done;
+   }
 
-  Task[] tasks;
+   mapping(uint256 => Task) tasks;
+   uint256[] taskIds;
+   uint256 lastTaskId = 1;
 
-  constructor() public {
-  }
+   constructor() public {}
 
-  function createTask(string memory _content, string memory _author) public {
-    tasks.push(Task(tasks.length, block.timestamp, _content, _author, false));
-  }
+   event TaskCreated(
+      uint256 id,
+      uint256 date,
+      string content,
+      string author,
+      bool done
+   );
 
-  function getTask(uint id) public view returns(uint, uint, string memory, string memory, bool) {
-    return (id, tasks[id].date, tasks[id].content, tasks[id].author, tasks[id].done);
-  }
+   function createTask(string memory _content, string memory _author) public {
+      lastTaskId++;
+      tasks[lastTaskId] = Task(
+         lastTaskId,
+         block.timestamp,
+         _content,
+         _author,
+         false
+      );
+      taskIds.push(lastTaskId);
+      emit TaskCreated(lastTaskId, block.timestamp, _content, _author, false);
+   }
 
+   function getTask(uint256 id)
+      public
+      view
+      returns (
+         uint256,
+         uint256,
+         string memory,
+         string memory,
+         bool
+      )
+   {
+      return (
+         id,
+         tasks[id].date,
+         tasks[id].content,
+         tasks[id].author,
+         tasks[id].done
+      );
+   }
+
+   function getTaskIds() public view returns (uint256[] memory) {
+      return taskIds;
+   }
 }
